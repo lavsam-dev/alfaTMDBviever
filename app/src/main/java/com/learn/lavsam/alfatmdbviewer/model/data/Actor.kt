176fc229257a1e7a50ec039e6e531ae4b3b2ec73
lavsam-dev <lavsam@gmail.com>
2021-08-29 10:52:31 +0400

    Добавлен googlemapsfragment. Готово. Добавлены актеры.

--- app/src/main/java/com/learn/lavsam/alfatmdbviewer/model/data/Actor.kt
+++ app/src/main/java/com/learn/lavsam/alfatmdbviewer/model/data/Actor.kt
@@ -18,6 +18,14 @@ data class Actor(
 fun getActorsList() = listOf(
     Actor(2461, "Mel Gibson", 2, "1956-01-03", "",
         "Peekskill, New York, USA", "/uHldNnlyi73x9hoZBGjISGoh908.jpg", 18.032),
+    Actor(18897, "Jackie Chan", 2, "1954-04-07", "",
+        "Victoria Peak, Hong Kong", "/nraZoTzwJQPHspAVsKfgl3RXKKa.jpg", 34.606),
     Actor(6384, "Keanu Reeves", 2, "1964-09-02", "",
-        "Beirut, Lebanon", "/rRdru6REr9i3WIHv2mntpcgxnoY.jpg", 35.002)
+        "Beirut, Lebanon", "/rRdru6REr9i3WIHv2mntpcgxnoY.jpg", 35.002),
+    Actor(1318905, "Lyubov Aksyonova", 1, "1990-03-15", "",
+        "Moscow, USSR (Russia)", "/yOQX4JPwX1uhMyVT1jJigAk7iUr.jpg", 25.334),
+    Actor(63, "Milla Jovovich", 1, "1975-12-17", "",
+        "Kiev, Ukraine", "/usWnHCzbADijULREZYSJ0qfM00y.jpg", 20.022),
+    Actor(2460109, "Misato Morita", 1, "1996-09-13", "",
+        "Kanagawa Prefecture, Japan", "/rS8y85AhCkQm5L13Qzn2dWT5Yfe.jpg", 11.89)
 )
